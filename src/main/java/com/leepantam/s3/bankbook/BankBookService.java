e6175df696ac82d3eae8177cc4510ea0195bcbc0
Leepantam <nighfivego@gmail.com>
2021-03-24 10:43:06 +0900

    Create BankBookService.java

---
+++ src/main/java/com/leepantam/s3/bankbook/BankBookService.java
@@ -0,0 +1,22 @@
+package com.leepantam.s3.bankbook;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BankBookService {
+	
+	@Autowired
+	private BankBookDAO bDao;
+	
+	public List<BankBookDTO> getList() throws Exception {
+		return bDao.getList();
+	}
+	
+	public BankBookDTO getSelect(BankBookDTO bDto) throws Exception {
+		return bDao.getSelect(bDto);
+	}
+	
+}
