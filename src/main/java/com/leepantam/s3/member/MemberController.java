cc7b5912e7cde68638ae49812c6a1c9e67140523
Leepantam <nighfivego@gmail.com>
2021-03-25 14:26:23 +0900

    memberUpdate

--- src/main/java/com/leepantam/s3/member/MemberController.java
+++ src/main/java/com/leepantam/s3/member/MemberController.java
@@ -11,64 +11,67 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value="/member/**")
 public class MemberController {
-	
+
 	@Autowired
 	private MemberService mServ;
-	
+
 	@RequestMapping(value="memberJoin")
 	public void memberJoin() {
-		
+
 	}
-	
+
 	@RequestMapping(value="memberJoin",method=RequestMethod.POST)
 	public String memberJoin(MemberDTO mDto) throws Exception {
 		int result=mServ.memberJoin(mDto);
 		return "redirect:../";
 	}
-	
-	
+
+
 	@RequestMapping(value="memberLogin")
 	public void memberLogin() {
-		
+
 	}
-	
+
 	@RequestMapping(value="memberLogin", method=RequestMethod.POST)
 	public String memberLogin(MemberDTO mDto,HttpSession session) throws Exception {
 		mDto = mServ.memberLogin(mDto);
 		session.setAttribute("member", mDto);
 		return "redirect:../";
 	}
-	
+
 	@RequestMapping(value="memberLogout")
 	public String memberLogout(HttpSession session) throws Exception {
 		session.invalidate();
 		return "redirect:../";
 	}
-	
+
 	@RequestMapping(value="memberPage")
 	public void memberPage() throws Exception{
-		
+
 	}
-	
+
 	@RequestMapping(value="memberDelete")
 	public String memberDelete(HttpSession session) throws Exception{
 		int result = mServ.memberDelete((MemberDTO)session.getAttribute("member"));
-		
+
 		session.invalidate();
-		
+
 		return "redirect:../";
 	}
-	
+
 	@RequestMapping(value="memberUpdate")
 	public void memberUpdate() throws Exception{
-		
+
 	}
-	
+
 	@RequestMapping(value="memberUpdate", method=RequestMethod.POST)
 	public String memberUpdate(MemberDTO mDto, HttpSession session) throws Exception{
 		int result = mServ.memberUpdate(mDto);
-		session.setAttribute("member", mDto);
-		
+
+		if(result>0) {			
+			session.setAttribute("member", mDto);
+		}
+
 		return "redirect:../";
 	}
 }
